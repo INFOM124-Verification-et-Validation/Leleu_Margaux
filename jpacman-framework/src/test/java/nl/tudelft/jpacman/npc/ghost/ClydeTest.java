07c077fb80452b01b8613dbeb9b855a9d32473ed
Maragaux <100001703+Maragaux@users.noreply.github.com>
2024-10-23 14:39:02 +0200

    Clyde

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -1,19 +1,28 @@
 package nl.tudelft.jpacman.npc.ghost;
 
-import nl.tudelft.jpacman.board.BasicUnit;
+
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.board.Direction;
+import nl.tudelft.jpacman.level.LevelFactory;
 import nl.tudelft.jpacman.npc.Ghost;
+import nl.tudelft.jpacman.npc.ghost.GhostFactory;
 import nl.tudelft.jpacman.sprite.PacManSprites;
-import nl.tudelft.jpacman.ghost.Clyde;
+import nl.tudelft.jpacman.npc.ghost.Clyde;
 import nl.tudelft.jpacman.sprite.SpriteStore;
+import nl.tudelft.jpacman.level.MapParser;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 
 public class ClydeTest {
-    PacManSprites SPRITE_STORE = new PacManSprites();
-    GhostFactory factory= new GhostFactory(SPRITE_STORE);
-    Clyde clyde= factory.createClyde();
-
-
-
-
+    private MapParser parser;
+    @BeforeEach
+    void setUp() {
+        PacManSprites sprites = new PacManSprites();
+        parser = new MapParser(new LevelFactory(sprites, new GhostFactory(
+            sprites)), new BoardFactory(sprites));
+//        parser.parseMap()
+    }
 }
